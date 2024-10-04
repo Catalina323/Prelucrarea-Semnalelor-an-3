e7b3e8875ee4bf78db39a2fe40b4945b77f1eadc
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2024-10-04 14:07:34 +0300

    Update ex1.py

--- Lab 1/ex1.py
+++ Lab 1/ex1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # a)
-time = np.arange(0, 0.03, 0.0005)
+time = np.arange(0, 0.1, 0.0005)
 
 # b)
 def x(t):
@@ -16,30 +16,34 @@ x_rez = x(time)
 y_rez = y(time)
 z_rez = z(time)
 
-# fig, axs = plt.subplots(3)
-# fig.subtitle("titlu")
-# axs[0].plot(time, x_rez)
-# axs[1].plot(time, y_rez)
-# axs[2].plot(time, z_rez)
-# fig.savefig("fig1.pdf")
-# plt.show()
-
+fig, axs = plt.subplots(3)
+fig.suptitle("semnalele x, y, z")
+axs[0].plot(time, x_rez)
+axs[1].plot(time, y_rez)
+axs[2].plot(time, z_rez)
+fig.savefig("fig1.pdf")
+plt.show()
 
 # c)
 frecv = 200
 T = 1/frecv
-aux = np.array([i * T for i in range(6)])
-x_es = np.array(x(aux))
-y_es = np.array(y(aux))
-z_es = np.array(z(aux))
+
+esantion = np.arange(0, 0.1, T)
+
+x_es = np.array(x(esantion))
+y_es = np.array(y(esantion))
+z_es = np.array(z(esantion))
 
 
 fig2, axs2 = plt.subplots(3)
-fig2.suptitle("titlu")
-axs2[0].stem(time, x_es)
-axs2[1].stem(time, y_es)
-axs2[2].stem(time, z_es)
+fig2.suptitle("semnalele x, y, z esantionate")
+axs2[0].stem(esantion, x_es)
+axs2[1].stem(esantion, y_es)
+axs2[2].stem(esantion, z_es)
+
+axs2[0].plot(time, x_rez)
+axs2[1].plot(time, y_rez)
+axs2[2].plot(time, z_rez)
+
 fig2.savefig("fig2.pdf")
 plt.show()
-
-
