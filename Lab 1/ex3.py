65108670ecfb7554be9ebbf330038ca11daf8b9e
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2024-10-08 11:39:27 +0300

    Create ex3.py

---
+++ Lab 1/ex3.py
@@ -0,0 +1,6 @@
+# ex 3
+
+# a) Din frecventa de 2000 Hz de esantionare vor rezulta 2000 de esantioane intr-o secunda, rezulta ca
+# distanta dintre 2 esantioane este de 1/2000 adica 0,0005 secunde
+
+# b) intr-o ora vom avea 2000 * 3600 = 7.200.000 esantioane care vor ocupa 28.800.000 biti = 3.600.000 bytes
