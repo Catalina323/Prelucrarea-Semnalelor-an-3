a22d6cc51d6e301523e0d30e980117087a85d193
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2024-10-12 11:27:44 +0300

    Update ex3.py

--- Lab 2/ex3.py
+++ Lab 2/ex3.py
@@ -5,10 +5,9 @@ import scipy.signal as sig
 import sounddevice as sd
 
 rate = int(10e4)
+
 # scipy.io.wavfile.write('nume.wav', rate, signal)
-
 # rate, x = scipy.io.wavfile.read('nume.wav')
-
 # fs = frecv de esantionare
 # fs = 44100
 # sounddevice.play(myarray, fs)
@@ -25,13 +24,15 @@ wav.write('nume0.wav', rate, sig0)
 # fs = 44100
 # sd.play(sig0, fs)
 
-sig_citit = wav.read("nume0.wav")
-
+rate_read, sig_citit = wav.read("nume0.wav")
 print((sig0 == sig_citit).all())
+
 # b)
 # time = np.arange(0, 3, 0.0005)
 # frecv_semnal = 800
 # sig1 = semnal(time, frecv_semnal)
+# sd.play(sig1, fs)
+
 
 # c)
 # def semnal_sawtooth(t):
@@ -41,11 +42,12 @@ print((sig0 == sig_citit).all())
 #
 # time = np.arange(0, 0.015, 0.00005)
 # sig2 = semnal_sawtooth(time)
+# sd.play(sig2, fs)
+
 
 # d)
 # def semnal_square(t):
 #     return np.sign(np.sin(2 * np.pi * 300 * t))
 # time = np.arange(0, 0.006, 0.00005)
 # sig3 = semnal_square(time)
-
-
+# sd.play(sig3, fs)
