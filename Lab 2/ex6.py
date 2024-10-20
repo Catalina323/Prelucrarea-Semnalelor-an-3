00b3673de9a716944f58eb80dbc0de0df92d96cb
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2024-10-20 15:22:08 +0300

    Update ex6.py

--- Lab 2/ex6.py
+++ Lab 2/ex6.py
@@ -6,7 +6,10 @@ def semnal_sin(t, f):
 
 
 fs = 2000
-time = np.arange(0, 0.05, 1 / fs)
+#time = np.arange(0, 0.05, 1 / fs)    # asta da bug :((((((
+time = np.linspace(0, 1, fs)
+
+time = time[0:150]
 
 sig0 = semnal_sin(time, fs / 2)
 sig1 = semnal_sin(time, fs / 4)
@@ -18,8 +21,3 @@ ax[1].plot(time, sig1)
 ax[2].plot(time, sig2)
 fig.savefig("ex6.pdf")
 plt.show()
-
-# pentru frecventa = fs / 2 vom avea 2 esantioane luate pentru fiecare perioada ceea ce este prea putin pentru a
-# reprezenta corect functia
-
-
