36ec659a217059bf0ad18f8e933e6200003983c0
Catalina323 <116120497+Catalina323@users.noreply.github.com>
2024-10-30 21:44:22 +0200

    Add files via upload

---
+++ Lab 4/ex7.py
@@ -0,0 +1,14 @@
+# P_semnal_dB = 90 dB
+# SNR_dB = 80 dB
+# P_zgomot = ?
+
+# SNR_dB = 10 * lg(SNR)
+# lg(SNR) = 8
+# SNR = 10**8
+# P_semnal = 10 ** (P_semnal_dB / 10) = 10**9
+# P_semnal / P_zgomot = 10**8
+# P_zgomot = P_semnal / 10**8
+# P_semnal = 10 dB
+
+
+
